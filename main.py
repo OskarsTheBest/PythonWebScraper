653d5f3e0240ff6abae1c0f33167baabfc159fdd
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-04-01 11:31:48 +0300

    defined new funcs, restyled code, scrapes first 30 github repos now

--- main.py
+++ main.py
@@ -1,98 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import unicodedata
+import os
 
 topics_url = 'https://github.com/topics'
+
+
 response = requests.get(topics_url)
-len(response.text)
-page_contents = response.text
-page_contents[:1000]
-with open('webpage.html', 'w', encoding='utf-8') as f:
-    f.write(page_contents)
-        
+page_contents = response.text        
 doc = BeautifulSoup(page_contents, 'html.parser')
 
 selected_title_tag = 'f3 lh-condensed mb-0 mt-1 Link--primary'
 selected_desc_tag = 'f5 color-fg-muted mb-0 mt-1'
 
 topic_title_tags =doc.find_all('p', {'class' : selected_title_tag})
-
 topic_desc_tags = doc.find_all('p', {'class' : selected_desc_tag})
-
 topic_link_tags = doc.find_all('a', {'class' : 'no-underline flex-grow-0'})
-
 
 
 topic0_url = "https://github.com" + topic_link_tags[0]['href']
 
-topic_titles = []
-
-for tag in topic_title_tags:
-    topic_titles.append(tag.text)
-    
-    
-
-
-try:
-    topic_descs = []
-
-    for tag in topic_desc_tags:
-        topic_descs.append(tag.text.strip())
-
-
-except Exception as e:
-    print(f"Error: {e}")
-    
 topic_urls = []
 base_url = 'https://github.com'
 for tag in topic_link_tags:
     topic_urls.append(base_url + tag['href'])
-
-
-
-topics_dict = {
-    'title': topic_titles,
-    'description': topic_descs,
-    'url': topic_urls
-}
-
-topic_df = pd.DataFrame(topics_dict).to_csv('topics.csv',index = None, encoding='utf-8')
-
 topic_page_url = topic_urls[0]
 
+
 response2 = requests.get(topic_page_url)
-
-
-page_contents2 = response2.text
-page_contents2[:1000]
-with open('topics.html', 'w', encoding='utf-8') as f:
-    f.write(page_contents2)
-        
+page_contents2 = response2.text 
 topic_doc = BeautifulSoup(page_contents2, 'html.parser')
 
 
-repo_h3_class = 'f3 color-fg-muted text-normal lh-condensed'
-repo_tags = topic_doc.find_all('h3', {'class' : repo_h3_class})
-a_tags = repo_tags[0].find_all('a')
 
-
-repo_url = base_url + a_tags[1]['href']
-
-
-star_span_class = 'Counter js-social-count'
-star_tags = topic_doc.find_all('span', { 'class': star_span_class})
-
-
-
-
-
-
-def parse_star_count(stars_str):
-    stars_str = stars_str.strip()
-    if stars_str[-1] == 'k':
-        return int(float(stars_str[:-1]) * 1000)
-    return int(stars_str)
 
     
     
@@ -104,6 +44,13 @@ def get_topic_page(topic_url):
     topic_doc = BeautifulSoup(response2.text, 'html.parser')
     return topic_doc
 
+
+
+def parse_star_count(stars_str):
+    stars_str = stars_str.strip()
+    if stars_str[-1] == 'k':
+        return int(float(stars_str[:-1]) * 1000)
+    return int(stars_str)
 
 
 
@@ -148,7 +95,14 @@ def get_topic_repos(topic_doc):
 
 
 
-get_topic_repos(get_topic_page(topic_urls[4])).to_csv('androidtopics.csv',index = None, encoding='utf-8')
+
+
+def scrape_topic(topic_url, path):
+    if os.path.exists(path):
+        print("The file {} already Exists. Skipped".format(path))
+        return
+    topic_df = get_topic_repos(get_topic_page(topic_url))
+    topic_df.to_csv(path, index=None, encoding='utf-8')
 
 
 
@@ -198,3 +152,15 @@ def scrape_topics():
     return pd.DataFrame(topics_dict)
     
 
+
+def scrape_topics_repos():
+    print("Scraping an list of the Topics in github")
+    topics_df = scrape_topics()
+    
+    os.makedirs('csv_files', exist_ok=True)
+    
+    for index, row in topics_df.iterrows():
+        print("Top Repositories are getting scraped for {}".format(row['title']))
+        scrape_topic(row['url'], 'csv_files/{}.csv'.format(row['title']))
+        
+scrape_topics_repos()
